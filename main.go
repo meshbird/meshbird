d92353eedee53fef0fe722aac2015d1d80db0878
Artem Andreenko <miolini@gmail.com>
2016-01-23 04:30:17 +0300

    add basic cmd tool

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+var (
+// VERSION var using for auto versioning through Go linker
+	VERSION = "dev"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "meshbird"
+	app.Usage = "distributed overlay private networking"
+	app.Version = VERSION
+	app.Commands = []cli.Command{
+		{
+			Name:    "new",
+			Aliases: []string{"n"},
+			Usage:   "create new network",
+			Action:  actionNew,
+		},
+		{
+			Name:    "join",
+			Aliases: []string{"r"},
+			Usage:   "join network",
+			Action:  actionJoin,
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		println("error: ", err)
+	}
+}
+
+func actionNew(ctx *cli.Context) {
+
+}
+
+func actionJoin(ctx *cli.Context) {
+
+}
