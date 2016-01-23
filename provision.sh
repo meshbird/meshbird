e3c91977237f16088b16e97cc8d0d68b1055fa2e
Maksim Naumov <maksim@naumov.org>
2016-01-23 21:31:05 +0100

    Fix vagrant

--- provision.sh
+++ provision.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install git
+apt-get update && apt-get install -y git
 curl -O https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
 tar -xvf go1.5.3.linux-amd64.tar.gz
 mv go /usr/local
@@ -9,7 +8,8 @@ rm -f go1.5.3.linux-amd64.tar.gz
 echo "" >> /home/vagrant/.profile
 echo "export PATH=\$PATH:/usr/local/go/bin:/home/vagrant/go/bin" >> /home/vagrant/.profile
 echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.profile
-mkdir -p /home/vagrant/go/src
+mkdir -p /home/vagrant/go/src/github.com/gophergala2016
 mkdir -p /home/vagrant/go/pkg
 mkdir -p /home/vagrant/go/bin
+ln -s /vagrant /home/vagrant/go/src/github.com/gophergala2016/meshbird
 chown -R vagrant:vagrant /home/vagrant/go
