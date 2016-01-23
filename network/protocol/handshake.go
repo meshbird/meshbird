31827394524291bbd716289f58a73de4df526807
Maksim Naumov <maksim@naumov.org>
2016-01-23 20:25:22 +0100

    Change handshake message

--- network/protocol/handshake.go
+++ network/protocol/handshake.go
@@ -1,44 +1,17 @@
 package protocol
 
 import (
-	"bytes"
-	"errors"
 	"io"
 )
 
-const (
-	handshakeKeyLen = 16
-)
-
-var (
-	ErrorUnknownHandshakeMagic    = errors.New("unknown handshace magic")
-	ErrorUnableToReadHandshakeKey = errors.New("unable to read hankshake key")
-
-	handshakeMagic = []byte{
-		'M', 'E', 'S', 'H',
-	}
-)
-
 type (
-	HandshakeMessage struct {
-		Message
-
-		Magic []byte
-		Key   []byte
-	}
+	HandshakeMessage []byte
 )
-
-func NewHandshakeMessage(key []byte) HandshakeMessage {
-	return HandshakeMessage{
-		Magic: handshakeMagic,
-		Key:   key,
-	}
-}
 
 func NewHandshakePacket(key []byte) *Packet {
 	body := Body{
 		Type: TypeHandshake,
-		Msg:  NewHandshakeMessage(key),
+		Msg:  HandshakeMessage(key),
 	}
 	return &Packet{
 		Head: Header{
@@ -50,33 +23,10 @@ func NewHandshakePacket(key []byte) *Packet {
 }
 
 func (m HandshakeMessage) Len() uint16 {
-	return uint16(len(handshakeMagic) + handshakeKeyLen)
+	return uint16(len(m))
 }
 
-func (m HandshakeMessage) WriteTo(w io.Writer) (n int64, err error) {
-	if _, err = w.Write(m.Magic); err != nil {
-		return
-	}
-	if _, err = w.Write(m.Key); err != nil {
-		return
-	}
-	n = int64(len(m.Magic) + len(m.Key))
-	return
-}
-
-func decodeHandshake(data []byte) (HandshakeMessage, error) {
-	var msg HandshakeMessage
-	reader := bytes.NewBuffer(data)
-
-	msg.Magic = reader.Next(len(handshakeMagic))
-	if !bytes.Equal(msg.Magic, handshakeMagic) {
-		return msg, ErrorUnknownHandshakeMagic
-	}
-
-	msg.Key = reader.Next(handshakeKeyLen)
-	if len(msg.Key) != handshakeKeyLen {
-		return msg, ErrorUnableToReadHandshakeKey
-	}
-
-	return msg, nil
+func (m HandshakeMessage) WriteTo(w io.Writer) (int64, error) {
+	n, err := w.Write(m)
+	return int64(n), err
 }
--- network/protocol/handshake_test.go
+++ network/protocol/handshake_test.go
@@ -8,37 +8,36 @@ import (
 )
 
 func TestDecodeHandShake(t *testing.T) {
-	key := []byte("nNJkE2LUGZ7AVpjw")
+	key := []byte("m7JC5ApnVKrTLs5L2Sz8SPMRckP2Wqkj")
 	data := []byte{
-		0, 21, // length
+		0, 33, // length
 		1,              // version
 		0,              // type
-		77, 69, 83, 72, // MESH
-		110, 78, 74, 107, 69, 50, 76, 85, 71, 90, 55, 65, 86, 112, 106, 119, // nNJkE2LUGZ7AVpjw
+		109, 55, 74, 67, 53, 65, 112, 110, 86, 75, 114, 84, 76, 115, 53, 76,
+		50, 83, 122, 56, 83, 80, 77, 82, 99, 107, 80, 50, 87, 113, 107, 106,
 	}
 
 	pack, err := protocol.Decode(data)
 	if assert.Nil(t, err) && assert.NotNil(t, pack) {
-		assert.Equal(t, uint16(21), pack.Head.Length)
+		assert.Equal(t, uint16(33), pack.Head.Length)
 		assert.Equal(t, uint8(1), pack.Head.Version)
 		assert.Equal(t, protocol.TypeHandshake, pack.Data.Type)
 		assert.Empty(t, pack.Data.Vector)
 		msg, ok := pack.Data.Msg.(protocol.HandshakeMessage)
 		if assert.True(t, ok) {
-			assert.Equal(t, []byte("MESH"), msg.Magic)
-			assert.Equal(t, key, msg.Key)
+			assert.Equal(t, key, []byte(msg))
 		}
 	}
 }
 
 func TestEncodeHandShake(t *testing.T) {
-	key := []byte("MCPqt8z2DcyhQzfj")
+	key := []byte("hM57uSygtTFwp4f7fpdy6fdEJEqqXrZh")
 	expected := []byte{
-		0, 21, // length
-		1,              // version
-		0,              // type
-		77, 69, 83, 72, // MESH
-		77, 67, 80, 113, 116, 56, 122, 50, 68, 99, 121, 104, 81, 122, 102, 106, // MCPqt8z2DcyhQzfj
+		0, 33, // length
+		1, // version
+		0, // type
+		104, 77, 53, 55, 117, 83, 121, 103, 116, 84, 70, 119, 112, 52, 102, 55,
+		102, 112, 100, 121, 54, 102, 100, 69, 74, 69, 113, 113, 88, 114, 90, 104,
 	}
 
 	pack := protocol.NewHandshakePacket(key)
