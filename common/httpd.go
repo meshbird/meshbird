80337d64a03a08f760258800cc6863188a9659d1
Gen1us2k <minkin.andrew@gmail.com>
2016-01-24 23:20:19 +0100

    Return json data

--- common/httpd.go
+++ common/httpd.go
@@ -1,7 +1,7 @@
 package common
 
 import (
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
@@ -13,7 +13,12 @@ type HttpService struct {
 
 	localnode *LocalNode
 	netTable  *NetTable
+	iface     *InterfaceService
 	logger    *log.Logger
+}
+type Response struct {
+	ifaceName   string `json:"iface"`
+	localIPAddr string `json:"local_ip_addr"`
 }
 
 func (hs *HttpService) Name() string {
@@ -22,17 +27,23 @@ func (hs *HttpService) Name() string {
 
 func (hs *HttpService) Init(ln *LocalNode) (err error) {
 	hs.logger = log.New(os.Stderr, "[httpd] ", log.LstdFlags)
+	hs.iface = ln.Service("iface").(*InterfaceService)
 	hs.localnode = ln
 	hs.netTable = ln.Service("net-table").(*NetTable)
 	return nil
 }
 
 func (hs *HttpService) Run() error {
-	http.HandleFunc("/stat", getStats)
+	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		resp := Response{hs.iface.instance.Name(), hs.localnode.State().PrivateIP.String()}
+		data, err := json.Marshal(resp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
 	http.ListenAndServe(":15080", nil)
 	return nil
-}
-
-func getStats(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello mazafaka")
 }
