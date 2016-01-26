c288caa0bee63667606ebfa28db09880f8e5d983
Artem Andreenko <miolini@gmail.com>
2016-01-27 00:25:58 +0300

    fix aes benchmarks

--- secure/crypt_test.go
+++ secure/crypt_test.go
@@ -49,7 +49,7 @@ func BenchmarkEncryptAesCbc(b *testing.B) {
 	}
 	b.StopTimer()
 	ts := time.Since(t)
-	b.Logf("encrypt speed: %.2f Mbit/s", float64(counter) * 8 / ts.Seconds() / 1024 / 1024 )
+	b.Logf("encryption speed: %.2f Mbit/s", float64(counter) * 8 / ts.Seconds() / 1024 / 1024 )
 }
 
 func BenchmarkDescryptAesCbc(b *testing.B) {
@@ -72,5 +72,5 @@ func BenchmarkDescryptAesCbc(b *testing.B) {
 	}
 	b.StopTimer()
 	ts := time.Since(t)
-	b.Logf("encrypt speed: %.2f Mbit/s", float64(counter) * 8 / ts.Seconds() / 1024 / 1024 )
+	b.Logf("decryption speed: %.2f Mbit/s", float64(counter) * 8 / ts.Seconds() / 1024 / 1024 )
 }
