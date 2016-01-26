cdc9771b7e3d046440d71893339571af8aa80bb0
Artem Andreenko <miolini@gmail.com>
2016-01-27 00:22:26 +0300

    aes benchmarks

--- secure/crypt_test.go
+++ secure/crypt_test.go
@@ -1,20 +1,76 @@
 package secure
 
-import "testing"
+import (
+	"testing"
+	"bytes"
+	"crypto/rand"
+	"time"
+)
+
+var (
+	original = []byte("Hello, World from MeshBird! Hello, World from MeshBird! Hello, World from MeshBird! Hello, World from MeshBird! ")
+)
 
 func TestEncryptIV(t *testing.T) {
 	key := randomBytes(16)
 	iv := randomBytes(16)
-	decrypted := []byte("Hello, World from MeshBird!")
-	encrypted, err := EncryptIV(decrypted, key, iv)
+	encrypted, err := EncryptIV(original, key, iv)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Logf("encrypted: %x", encrypted)
 
-	decrypted2, err := DecryptIV(encrypted, key, iv)
+	decrypted, err := DecryptIV(encrypted, key, iv)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Logf("decrypted: %s", string(decrypted2))
+	if !bytes.Equal(original, decrypted) {
+		t.Fatal("original payload not equals to encrypted/decrypted")
+	}
+}
+
+func BenchmarkEncryptAesCbc(b *testing.B) {
+	key := randomBytes(16)
+	iv := make([]byte, 16)
+	counter := 0
+	dataLen := len(original)
+	t := time.Now()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := rand.Read(iv)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_, err = EncryptIV(original, key, iv)
+		if err != nil {
+			b.Fatal(err)
+		}
+		counter += dataLen
+	}
+	b.StopTimer()
+	ts := time.Since(t)
+	b.Logf("encrypt speed: %.2f GB/s", float64(counter) / ts.Seconds() / 1024 / 1024 / 1024)
+}
+
+func BenchmarkDescryptAesCbc(b *testing.B) {
+	key := randomBytes(16)
+	iv := randomBytes(16)
+	encrypted, err := EncryptIV(original, key, iv)
+	if err != nil {
+		b.Fatal(err)
+	}
+	counter := 0
+	dataLen := len(encrypted)
+	t := time.Now()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := DecryptIV(encrypted, key, iv)
+		if err != nil {
+			b.Fatal(err)
+		}
+		counter += dataLen
+	}
+	b.StopTimer()
+	ts := time.Since(t)
+	b.Logf("encrypt speed: %.2f GB/s", float64(counter) / ts.Seconds() / 1024 / 1024 / 1024)
 }
