dddd66df7c1ad6f77ba0de6691602794d4f9e2df
Artem Andreenko <miolini@gmail.com>
2016-01-28 00:34:29 +0300

    fix linux iface

--- network/interface_linux.go
+++ network/interface_linux.go
@@ -6,6 +6,26 @@ import (
 	"strconv"
 )
 
+func interfaceOpen(ifType, ifName string) (*Interface, error) {
+	var err error
+	if ifType != "tun" && ifType != "tap" {
+		return nil, fmt.Errorf("unknown interface type: %s", ifType)
+	}
+	ifce := new(Interface)
+	for i := 0; i < 256; i++ {
+		ifPath := fmt.Sprintf("/dev/tun/%s%d", ifType, ifName)
+		ifce.file, err = os.OpenFile(ifPath, os.O_RDWR, 0644)
+		if err != nil {
+			continue
+		}
+		ifce.name = ifName
+	}
+	if ifce.file == nil {
+		return nil, fmt.Errorf("can't create network interface")
+	}
+	return ifce, err
+}
+
 func AssignIpAddress(iface string, IpAddr string) error {
 	err := exec.Command("ip", "addr", "add", IpAddr, "dev", iface).Run()
 	if err != nil {
