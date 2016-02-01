a50894d6cf50cdee1eab7db6825d1029f17ac670
Artem Andreenko <miolini@gmail.com>
2016-02-01 05:31:29 +0300

    add bytes.Buffer pool

--- network/bufpool.go
+++ network/bufpool.go
@@ -1,16 +1,23 @@
 package network
 
-import "sync"
+import (
+	"sync"
+	"bytes"
+)
 
 type BufPool struct {
 	pool sync.Pool
 }
 
+func newBytesSliceFunc(size int) func() interface{} {
+	return func() interface{} {
+		return make([]byte, size)
+	}
+}
+
 func NewBufPool(defaultSize int) *BufPool {
 	bp := &BufPool{}
-	bp.pool.New = func() interface{} {
-		return make([]byte, defaultSize)
-	}
+	bp.pool.New = newBytesSliceFunc(defaultSize)
 	return bp
 }
 
@@ -20,4 +27,26 @@ func (bp *BufPool) Put(buf []byte) {
 
 func (bp *BufPool) Get() []byte {
 	return bp.pool.Get().([]byte)
+}
+
+type BytesBufferPool struct {
+	pool sync.Pool
+}
+
+func newBytesBuffer() interface{} {
+	return &bytes.Buffer{}
+}
+
+func NewBytesBufferPool() *BytesBufferPool {
+	bp := &BytesBufferPool{}
+	bp.pool.New = newBytesBuffer
+	return bp
+}
+
+func (bbp *BytesBufferPool) Put(buf *bytes.Buffer) {
+	bbp.pool.Put(buf)
+}
+
+func (bbp *BytesBufferPool) Get() *bytes.Buffer {
+	return bbp.pool.Get().(*bytes.Buffer)
 }
