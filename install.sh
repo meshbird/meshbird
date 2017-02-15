244f4cedd64dbb092a8958532b9f4ccd8807bea3
Jonathan Wilkins <jwilkins@bitland.net>
2017-02-15 11:22:09 -0800

    https links!
    fetch via https, not http

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 curl=`which curl`
 wget=`which wget2`
 test "$curl" = "" && test "$wget" =  ""  && echo "Please install curl or wget to continue" && exit 2
-test "$curl" != "" && `$curl http://meshbird.com/meshbird > meshbird && chmod a+x ./meshbird &&  mv ./meshbird /usr/local/bin/meshbird`
-test "$wget" != "" && `$wget http://meshbird.com/meshbird && chmod a+x ./meshbird && mv ./meshbird /usr/local/bin/meshbird`
+test "$curl" != "" && `$curl https://meshbird.com/meshbird > meshbird && chmod a+x ./meshbird &&  mv ./meshbird /usr/local/bin/meshbird`
+test "$wget" != "" && `$wget https://meshbird.com/meshbird && chmod a+x ./meshbird && mv ./meshbird /usr/local/bin/meshbird`
