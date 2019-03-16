75cb30699abadeba0123c2a66dbd3f4f3b5ef398
Artem Andreenko <artemandreenko@Artems-MacBook-Pro.local>
2019-03-15 22:43:05 -0400

    platform fix

--- src/meshbird/iface/iface_linux.go
+++ src/meshbird/iface/iface_linux.go
@@ -10,21 +10,6 @@ import (
 	"github.com/songgao/water"
 )
 
-type Iface struct {
-	name string
-	ip   string
-	mtu  int
-	ifce *water.Interface
-}
-
-func New(name, ip string, mtu int) *Iface {
-	return &Iface{
-		name: name,
-		ip:   ip,
-		mtu:  mtu,
-	}
-}
-
 func (i *Iface) Start() error {
 	ip, netIP, err := net.ParseCIDR(i.ip)
 	if err != nil {
@@ -50,16 +35,4 @@ func (i *Iface) Start() error {
 		return fmt.Errorf("ifconfig run err: %s %s", err, string(output))
 	}
 	return nil
-}
-
-func (i *Iface) Name() string {
-	return i.ifce.Name()
-}
-
-func (i *Iface) Read(pkt PacketIP) (int, error) {
-	return i.ifce.Read(pkt)
-}
-
-func (i *Iface) Write(pkt PacketIP) (int, error) {
-	return i.ifce.Write(pkt)
 }
